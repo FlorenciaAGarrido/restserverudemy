79d287d4e4e1da05fa070e7d50faa13985422bbf
Florencia Garrido <flhor.garrido@gmail.com>
2022-10-10 02:53:47 -0300

    creando usuario en coleccion de base de datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response, request } = require('express');
+const Usuario = require('../models/usuario')
 
 const usuariosGet = (req = request, res = response ) => {
 
@@ -14,13 +15,16 @@ const usuariosGet = (req = request, res = response ) => {
     });
 };
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async (req, res = response) => {
 
     const body = req.body;
+    const usuario = new Usuario( body );
+
+    await usuario.save();
 
     res.json({
         msg: 'post API - controlador',
-        body
+        usuario
     });
 };
 
